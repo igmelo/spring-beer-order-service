ee6e3281f0ecef7bbd02d3f4da472c6e91add84e
Igor Melo <igorgalvao@gec.inatel.br>
2021-02-25 12:15:07 -0300

    Adding sql init file

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-23T14:40:45-0300",
+    date = "2021-02-25T11:58:07-0300",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
 )
 @Component
