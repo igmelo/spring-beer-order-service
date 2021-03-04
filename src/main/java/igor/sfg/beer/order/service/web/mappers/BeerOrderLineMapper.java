092b82a6ed3101526edc49647eb5ca5f2b13d845
Igor Melo <igorgalvao@gec.inatel.br>
2021-03-04 14:31:49 -0300

    Configuring Jms - closes # 3

---
+++ src/main/java/igor/sfg/beer/order/service/web/mappers/BeerOrderLineMapper.java
@@ -0,0 +1,14 @@
+package igor.sfg.beer.order.service.web.mappers;
+
+import igor.sfg.beer.order.service.domain.BeerOrderLine;
+import igor.sfg.beer.order.service.web.model.BeerOrderLineDto;
+import org.mapstruct.DecoratedWith;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+@DecoratedWith(BeerOrderLineMapperDecorator.class)
+public interface BeerOrderLineMapper {
+    BeerOrderLineDto beerOrderLineToDto(BeerOrderLine line);
+
+    BeerOrderLine dtoToBeerOrderLine(BeerOrderLineDto dto);
+}
---
+++ target/generated-sources/annotations/igor/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -0,0 +1,28 @@
+package igor.sfg.beer.order.service.web.mappers;
+
+import igor.sfg.beer.order.service.domain.BeerOrderLine;
+import igor.sfg.beer.order.service.web.model.BeerOrderLineDto;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-02-25T14:07:10-0300",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerOrderLineMapperImpl extends BeerOrderLineMapperDecorator implements BeerOrderLineMapper {
+
+    @Autowired
+    @Qualifier("delegate")
+    private BeerOrderLineMapper delegate;
+
+    @Override
+    public BeerOrderLine dtoToBeerOrderLine(BeerOrderLineDto dto)  {
+        return delegate.dtoToBeerOrderLine( dto );
+    }
+}
