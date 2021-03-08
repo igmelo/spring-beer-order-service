21e7b4384c8a432f02f8f8d07e176a32b6b3f3e5
Igor Melo <igorgalvao@gec.inatel.br>
2021-03-08 12:00:43 -0300

    Fixing CI build - closes #14

--- target/generated-sources/annotations/igor/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/igor/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-05T15:18:41-0300",
+    date = "2021-03-08T12:00:14-0300",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
 )
 @Component
