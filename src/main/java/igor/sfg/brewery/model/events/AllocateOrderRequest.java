b95a490cdb0da0e34a65060354ec4a3f633546ca
Igor Melo <igorgalvao@gec.inatel.br>
2021-03-08 11:39:51 -0300

    Allocating order - closes #13

---
+++ src/main/java/igor/sfg/brewery/model/events/AllocateOrderRequest.java
@@ -0,0 +1,15 @@
+package igor.sfg.brewery.model.events;
+
+import igor.sfg.brewery.model.BeerOrderDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AllocateOrderRequest {
+    private BeerOrderDto beerOrderDto;
+}
